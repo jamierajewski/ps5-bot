b5fb88f47809b939cb0213174eb447672893cfc8
Jamie Rajewski <jamie.rajewski1@gmail.com>
2021-06-23 15:05:36 -0600

    Add site classes

---
+++ sites.py
@@ -0,0 +1,15 @@
+
+class Site:
+    '''Defines a generic interface to hold site details
+    '''
+
+    def __init__(self):
+        pass
+
+
+class Costco(Site):
+    '''costco.ca interface
+    '''
+
+    def __init__(self):
+        super().__init__()
